5d2061b8a6ede0a66a046cbcecdb6b51e348b9c3
Ryan Soeder <mr.soeder@gmail.com>
2020-08-03 15:58:35 -0400

    ajax function added

--- ajaxboilerplate.js
+++ ajaxboilerplate.js
@@ -2,11 +2,25 @@ const xhr = new XMLHttpRequest();
 const url = 'https://api-to-call.com/endpoint';
 
 xhr.responseType = 'json';
+xhr.open('GET', url);
 xhr.onreadystatechange = function() {
   if (xhr.readyState === 4 && xhr.status === 200) {
     console.log(xhr.response);
   }
 };
 
-xhr.open('GET', url);
 xhr.send();
+
+
+// Function to make an AJAX request & return the data
+function getJSON(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.onload = () => {
+    if(xhr.status === 200) {
+      let data = JSON.parse(xhr.responseText);
+      return callback(data);
+    }
+  };
+  xhr.send();
+}
